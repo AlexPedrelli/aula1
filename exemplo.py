74966b61101176c43f92ec654cff5b9bcea1a1a4
ALEX SANDRO JOSÉ PEDRELLI <alex_pedrelli@estudante.sesisenai.org.br>
2025-10-03 11:21:59 -0300

    aula2 : exemplo de entrada do usuario

---
+++ exemplo.py
@@ -0,0 +1,11 @@
+
+nome = input("digite seu nome :")
+print("olá, ", nome)
+
+quantidade = int(input("digite a quantidade de pessoas na sala: "))
+
+print("ha", quantidade, "pessoas na sala", type(quantidade))
+
+outra_turma = 20
+soma = quantidade + outra_turma
+print("a soma é", soma)
