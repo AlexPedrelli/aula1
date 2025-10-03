fa5043e7e831c9f6be6bcb3fd58cf18e75644cc5
ALEX SANDRO JOSÉ PEDRELLI <alex_pedrelli@estudante.sesisenai.org.br>
2025-10-03 11:18:19 -0300

    aula2: variaveis e tipo

---
+++ aula2.py
@@ -0,0 +1,24 @@
+#trabalhando com variaveis
+#definindo uma variavel com o meu nome
+nome = "alex"
+print(" o nome é", nome)
+palavra = "xulofimpola"
+letra = "A"
+quantidade = 10
+valor = 10.5
+aberto = False
+
+print("o valor de palavra é", palavra, end="")
+print("(tipo: ", type(palavra),")")
+
+print("o valor de valor", valor, end="")
+print("(tipo: " , type(valor), ")")
+
+print("o valor de quantidade é", quantidade, end="")
+print("(tipo:", type(quantidade),")")
+
+print("o valor de letra é", letra, end="")
+print("(tipo:",type(letra),")")
+
+print("o valor de aberto é", aberto, end="")
+print("(tipo:",type(aberto),")")
