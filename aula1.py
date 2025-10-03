dbb5b168e45dd520a90e6aec3fbabdf40813e879
ALEX SANDRO JOSÉ PEDRELLI <alex_pedrelli@estudante.sesisenai.org.br>
2025-10-03 09:48:24 -0300

    aula 1: commit print

---
+++ aula1.py
@@ -0,0 +1,8 @@
+#escapei da maldicao de novo
+print("ola mundo")
+print("me chamo alex pedrelli")
+print("voce é meu amigo")
+print("esse é o curso de:", end="")
+print("logica", "de", "programacao")
+print("frase","com","hifen",sep="-")
+
